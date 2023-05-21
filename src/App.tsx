87912967ac2f6c7e9ebfac704b4f39752694d5a4
nicsylvia <nicsylvia15f@gmail.com>
2023-05-20 23:27:01 -0700

    saved changes

--- src/App.tsx
+++ src/App.tsx
@@ -16,17 +16,59 @@
 // export default App;
 
 import React from "react";
-import { RouterProvider } from "react-router-dom";
-import { mainRoute } from "./routers/mainRoute";
-import Section from "./components/Block/Section";
+import styled from "styled-components";
+
+//
 
 const App = () => {
   return (
     <div>
-      {/* <RouterProvider router={mainRoute} /> */}
-      <Section />
+      
+<House>
+  <Main>
+    
+<Block></Block>
+<Block3></Block3>
+<h1>BNXN.</h1>
+  </Main>
+</House>
+    
     </div>
   );
 };
 
 export default App;
+
+
+const Block3 =styled.div`
+width: 40%;
+height: 58vh;
+background-color: #ff0000;
+justify-content: center;
+
+`
+const Block =styled.div`
+width: 40%;
+height: 58vh;
+background-color: #000;
+justify-content: center;
+/* position: absolute; */
+`
+const House =styled.div`
+width: 100%;
+height: 100vh;
+background-color: red;
+justify-content: center;
+display: flex;
+align-items: center;
+
+`
+const Main =styled.div`width: 100%;
+width: 90%;
+height: 58vh;
+background-color: blue;
+justify-content: space-between;
+display: flex;
+align-items: center;
+position:absolute;
+`
